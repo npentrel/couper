c102349286ed68239da4de11648685d23840367f
Charlie Kingston <charlie@charliekingston.co.uk>
2016-02-27 17:34:19 +0000

    Minor changes to redundant code in 'listall.js'.

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,31 +1,22 @@
 chrome.contextMenus.create({
-  'title': 'Insert Best Coupon',
-  'contexts': ['link'],
-  'onclick': onClickInsertHandler
+    'title': 'Insert Best Coupon',
+    'contexts': ['link'],
+    'onclick': onClickInsertHandler
 });
 
 function onClickInsertHandler(info) {
-
+    alert("Hello, World")
 }
 
-
 chrome.contextMenus.create({
-  'title': 'Find All Coupons',
-  'contexts': ['link'],
-  'onclick': onClickListAllHandler
+    'title': 'Find All Coupons',
+    'contexts': ['link'],
+    'onclick': onClickListAllHandler
 });
 
 function onClickListAllHandler(info) {
-  var details = {};
-
-  details.title = info.selectionText;
-  details.url = info.linkUrl;
-  details.editable = info.editable;
-  details.id = info.menuItemId;
-  details.pageUrl = info.pageUrl;
-  details.text = info.selectionText;
-
-  chrome.windows.create({ url: 'listall.html', type: 'popup', width: 300, height: 500 }, function() {
-    chrome.runtime.sendMessage({ details: details }, function(response) {});
-  });
+    chrome.windows.create({url: 'listall.html', type: 'popup', width: 300, height: 500}, function () {
+        chrome.runtime.sendMessage({details: details}, function (response) {
+        });
+    });
 }
