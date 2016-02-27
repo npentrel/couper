15e2d842ddf9bdc475a1330297fd5c84446f88f5
npentrel <npentrel@gmail.com>
2016-02-27 16:01:00 +0000

    server returns voucher codes

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ install
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
+pip install beautifulsoup4
 ```
 
 run
--- server.py
+++ server.py
@@ -1,9 +1,25 @@
 from flask import Flask
+import requests
+import re
+from html.parser import HTMLParser
+import bs4
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    response = requests.get('http://www.myvouchercodes.co.uk/boots')
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    
+    codes = []
+    for p in soup.findAll('div'):
+	    code = p.find('div',{'class':'Offer-code'})
+	    if code:
+	        if code.text not in codes: 
+		        codes.append(code.text)
+
+    print (codes)
+    return ' '.join(codes)
 
 if __name__ == '__main__':
     app.run()
+
